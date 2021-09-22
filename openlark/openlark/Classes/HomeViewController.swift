3c18f0b131edaf43d91d1145fe6ffc12c1e3ed07
thinkcly <thinkcly@gmail.com>
2021-09-22 19:26:32 +0800

    feat: add check-in logic

--- openlark/openlark/Classes/HomeViewController.swift
+++ openlark/openlark/Classes/HomeViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    let checkinDate = "2021-09-23 17:21:00"   // year-month-day hour:minute:second
+    let delayTime = 10
+    var larkIsOpen: ((_ result: Bool) -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         self.view.backgroundColor = .gray
     }
@@ -19,8 +22,64 @@ class HomeViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        if let url = URL(string: "feishu://") {
-            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        let result = performCheckIn()
+        if result == false {
+            // retry check-in after a few second
         }
+        
+        self.larkIsOpen = { (result) in
+            switch result {
+            case true:
+                // check-in successful
+                break
+            case false:
+                // check-in failed
+                break
+            }
+        }
+    }
+    
+
+    func performCheckIn() -> Bool {
+        let checkInTime = getTimeIntervalFrom(checkinDate)
+        let timeInterval = getCurrentDate()
+        let differ = timeInterval - checkInTime
+        
+        guard differ > delayTime else {
+            return false
+        }
+        openLark()
+        return true
+    }
+}
+
+extension HomeViewController {
+    
+    func getCurrentDate() -> Int {
+        let timeInterval = Int(Date().timeIntervalSince1970)
+        return timeInterval
+    }
+    
+    func getTimeIntervalFrom(_ string: String) -> Int {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        formatter.timeZone = TimeZone(identifier: "Asia/Shanghai")
+        
+        guard let date = formatter.date(from: string) else {
+            return Int(Date().timeIntervalSince1970)
+        }
+        
+        let timeInterval = Int(date.timeIntervalSince1970)
+        return timeInterval
+    }
+    
+    func openLark() {
+        guard let url = URL(string: "feishu://") else {
+            return
+        }
+        
+        UIApplication.shared.open(url, options: [:], completionHandler: { [self] (result) in
+            larkIsOpen?(result)
+        })
     }
 }
