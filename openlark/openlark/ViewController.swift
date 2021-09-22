e5beb38d28c07001c865f7abe0c2c31cea0f5d10
thinkcly <thinkcly@gmail.com>
2021-09-22 14:32:14 +0800

    add openurl

--- openlark/openlark/ViewController.swift
+++ openlark/openlark/ViewController.swift
@@ -12,8 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .gray
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let url = URL(string: "feishu://") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
 }
 
