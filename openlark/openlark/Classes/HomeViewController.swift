36dbc345a016063a5b35ca90f819355f2d87aafe
thinkcly <thinkcly@gmail.com>
2021-09-22 14:47:20 +0800

    delete main.storyBoard and sceneDelegate

--- openlark/openlark/Classes/HomeViewController.swift
+++ openlark/openlark/Classes/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  openlark
 //
 //  Created by AlexChen on 2021/09/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,4 +24,3 @@ class ViewController: UIViewController {
         }
     }
 }
-
